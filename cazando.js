901807979d88ec362edd25701fde4e46eb25b507
ianm1610 <ianmetem1610@gmail.com>
2026-04-13 15:08:20 -0500

    Estructura de iniciarJuego con variables y constantes

--- cazando.js
+++ cazando.js
@@ -1,19 +1,35 @@
+let gatoX = 0;
+let gatoY = 0;
+let comidaX = 0;
+let comidaY = 0;
+
+const ALTO_GATO = 50;
+const ANCHO_GATO = 50;
+const ALTO_COMIDA = 30;
+const ANCHO_COMIDA = 30;
+
 let canvas = document.getElementById("areaJuego");
 let ctx = canvas.getContext("2d");
 
-function ejecutarAlCargar() {
+
+function iniciarJuego() {
+    gatoX = (canvas.width / 2) - (ANCHO_GATO / 2);
+    gatoY = (canvas.height / 2) - (ALTO_GATO / 2);
+
+    
+    comidaX = canvas.width - ANCHO_COMIDA;
+    comidaY = canvas.height - ALTO_COMIDA;
+
     graficarGato();
     graficarComida();
 }
 
-
 function graficarGato() {
     ctx.fillStyle = "blue";
-    ctx.fillRect(225, 225, 50, 50); 
+    ctx.fillRect(gatoX, gatoY, ANCHO_GATO, ALTO_GATO);
 }
-
 
 function graficarComida() {
     ctx.fillStyle = "red";
-    ctx.fillRect(0, 0, 30, 30);
+    ctx.fillRect(comidaX, comidaY, ANCHO_COMIDA, ALTO_COMIDA);
 }
